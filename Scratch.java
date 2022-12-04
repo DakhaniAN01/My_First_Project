dcba54b4a7595d13d1097fcf2268b528dc55ae6d
Adnan Dakhani <dakhani.an01@gmail.com>
2022-12-04 23:34:24 +0530

    commit 1

--- Scratch.java
+++ Scratch.java
@@ -1,5 +1,8 @@
 class Scratch {
     public static void main(String[] args) {
         System.out.println("Hello Adnan");
+System.out.println("Hello Adnan");
+System.out.println("Hello Adnan");
+System.out.println("Hello Adnan");
     }
 }
