7f3dcfbee5ac8f2af9ca914bd1daa223da6ff0ad
Adnan Dakhani <dakhani.an01@gmail.com>
2023-04-11 15:55:17 +0530

    added kazi and niyajoddin

--- Scratch.java
+++ Scratch.java
@@ -2,7 +2,7 @@ class Scratch {
     public static void main(String[] args) {
         System.out.println("Hello Adnan");
 System.out.println("Hello Adnan");
-System.out.println("Hello Adnan");
-System.out.println("Hello Adnan");
+System.out.println("Hello Niyajoddin");
+System.out.println("Hello Kazi");
     }
-}
+}   
